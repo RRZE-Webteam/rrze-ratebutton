395f58151cd0b7a3a135db04e7a18cbdd0ea5a79
Rolf <rolf.v.d.forst@fau.de>
2019-01-24 09:14:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### RRZE Rate Button
+
+Schaltung eines Buttons um einen Artikel oder eine Seite zu werten. Datenschutzkonform und ohne Performanceprobleme.
+
+#### Shortcode
+```sh
+[ratebutton]
+```
---
+++ rrze-ratebutton.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * Plugin Name:     RRZE Rate Button
+ * Plugin URI:      https://github.com/RRZE-Webteam/rrze-ratebutton
+ * Description:     Schaltung eines Buttons um einen Artikel oder eine Seite zu werten. Datenschutzkonform und ohne Performanceprobleme.
+ * Version:         1.0.0
+ * Author:          RRZE-Webteam
+ * Author URI:      https://blogs.fau.de/webworking/
+ * License:         GNU General Public License v2
+ * License URI:     http://www.gnu.org/licenses/gpl-2.0.html
+ * Domain Path:     /languages
+ * Text Domain:     rrze-ratebutton
+ */
+
+namespace RRZE\RateButton;
+
+use RRZE\RateButton\Main;
+
+defined('ABSPATH') || exit;
+
+const RRZE_PHP_VERSION = '7.1';
+const RRZE_WP_VERSION = '5.0';
+
+register_activation_hook(__FILE__, 'RRZE\RateButton\activation');
+register_deactivation_hook(__FILE__, 'RRZE\RateButton\deactivation');
+
+add_action('plugins_loaded', 'RRZE\RateButton\loaded');
+
+/*
+ * Einbindung der Sprachdateien.
+ * @return void
+ */
+function load_textdomain()
+{
+    load_plugin_textdomain('rrze-ratebutton', false, sprintf('%s/languages/', dirname(plugin_basename(__FILE__))));
+}
+
+/*
+ * Wird durchgeführt, nachdem das Plugin aktiviert wurde.
+ * @return void
+ */
+function activation()
+{
+    // Sprachdateien werden eingebunden.
+    load_textdomain();
+
+    // Überprüft die minimal erforderliche PHP- u. WP-Version.
+    system_requirements();
+
+    // Ab hier können die Funktionen hinzugefügt werden,
+    // die bei der Aktivierung des Plugins aufgerufen werden müssen.
+    // Bspw. wp_schedule_event, flush_rewrite_rules, etc.
+}
+
+/*
+ * Wird durchgeführt, nachdem das Plugin deaktiviert wurde.
+ * @return void
+ */
+function deactivation()
+{
+    // Hier können die Funktionen hinzugefügt werden, die
+    // bei der Deaktivierung des Plugins aufgerufen werden müssen.
+    // Bspw. wp_clear_scheduled_hook, flush_rewrite_rules, etc.
+}
+
+/*
+ * Überprüft die minimal erforderliche PHP- u. WP-Version.
+ * @return void
+ */
+function system_requirements()
+{
+    $error = '';
+
+    if (version_compare(PHP_VERSION, RRZE_PHP_VERSION, '<')) {
+        $error = sprintf(__('Your server is running PHP version %s. Please upgrade at least to PHP version %s.', 'rrze-ratebutton'), PHP_VERSION, RRZE_PHP_VERSION);
+    }
+
+    if (version_compare($GLOBALS['wp_version'], RRZE_WP_VERSION, '<')) {
+        $error = sprintf(__('Your Wordpress version is %s. Please upgrade at least to Wordpress version %s.', 'rrze-ratebutton'), $GLOBALS['wp_version'], RRZE_WP_VERSION);
+    }
+
+    // Wenn die Überprüfung fehlschlägt, dann wird das Plugin automatisch deaktiviert.
+    if (!empty($error)) {
+        deactivate_plugins(plugin_basename(__FILE__), false, true);
+        wp_die($error);
+    }
+}
+
+/*
+ * Wird durchgeführt, nachdem das WP-Grundsystem hochgefahren
+ * und alle Plugins eingebunden wurden.
+ * @return void
+ */
+function loaded()
+{
+    // Sprachdateien werden eingebunden.
+    load_textdomain();
+
+    // Automatische Laden von Klassen.
+    autoload();
+}
+
+/*
+ * Automatische Laden von Klassen.
+ * @return void
+ */
+function autoload()
+{
+    require 'autoload.php';
+    return new Main(plugin_basename(__FILE__));
+}
